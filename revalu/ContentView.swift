69f3b1b8d4db30899d4c5fc410280275064b9314
hasan çiftçi <se.hasancifti@gmail.com>
2020-04-30 17:50:46 +0300

    Initial Commit

---
+++ revalu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  revalu
+//
+//  Created by hasan çiftçi on 30.04.2020.
+//  Copyright © 2020 hasan çiftçi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
